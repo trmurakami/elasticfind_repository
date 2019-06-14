2f402e1ad08c96df3fd2558fc33dad404b6ff32b
Tiago <tiago@bdpife2.sibi.usp.br>
2019-06-14 10:08:57 -0300

    Initial

---
+++ Locale/en_US/LC_MESSAGES/en.php
@@ -0,0 +1,397 @@
+<?php return array (
+  'domain' => NULL,
+  'plural-forms' => 'nplurals=2; plural=(n != 1);',
+  'messages' =>
+  array (
+    '' =>
+    array (
+      '' =>
+      array (
+        0 => 'Project-Id-Version: BDPI
+Report-Msgid-Bugs-To: tiago.murakami@st.sibi.usp.br
+Last-Translator: Tiago Murakami <tiago.murakami@dt.sibi.usp.br>
+Language-Team: SIBiUSP
+MIME-Version: 1.0
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
+POT-Creation-Date: 2018-01-15T15:15:13-02:00
+PO-Revision-Date: 2016-11-18
+Language: en_US
+Plural-Forms: nplurals=2; plural=(n != 1);
+',
+      ),
+      'Acesso ao documento' =>
+      array (
+        0 => 'Online access',
+      ),
+      'Acesso online à fonte' =>
+      array (
+        0 => 'Online access',
+      ),
+      'Afiliação dos autores externos normalizada' =>
+      array (
+        0 => 'Normalized affiliation',
+      ),
+      'Afiliação dos autores externos não normalizada' =>
+      array (
+        0 => 'Non normalized affiliation',
+      ),
+      'Agência de fomento' =>
+      array (
+        0 => 'Funding Agencies',
+      ),
+      'Ano' =>
+      array (
+        0 => 'Year',
+      ),
+      'Ano de publicação' =>
+      array (
+        0 => 'Date published',
+      ),
+      'Anterior' =>
+      array (
+        0 => 'Previous',
+      ),
+      'arquivos de texto integral' =>
+      array (
+        0 => 'full-text files',
+      ),
+      'ARTIGO DE JORNAL' =>
+      array (
+        0 => 'NEWS ITEM',
+      ),
+      'ARTIGO DE JORNAL-DEP/ENTR' =>
+      array (
+        0 => 'NEWS ITEM',
+      ),
+      'ARTIGO DE PERIODICO' =>
+      array (
+        0 => 'Article',
+      ),
+      'Assuntos' =>
+      array (
+        0 => 'Subjects',
+      ),
+      'Assuntos provenientes das teses' =>
+      array (
+        0 => 'Subjects from thesis',
+      ),
+      'Autores' =>
+      array (
+        0 => 'Authors',
+      ),
+      'autores vinculados à USP' =>
+      array (
+        0 => 'USP affiliated authors',
+      ),
+      'Autores USP' =>
+      array (
+        0 => 'USP affiliated authors',
+      ),
+      'Bases' =>
+      array (
+        0 => 'Sources',
+      ),
+      'Biblioteca Digital de Produção Intelectual da Universidade de São Paulo' =>
+      array (
+        0 => 'Digital Library of Intellectual Production of Universidade de São Paulo',
+      ),
+      'Busca avançada' =>
+      array (
+        0 => 'Advanced search',
+      ),
+      'Busca institucional' =>
+      array (
+        0 => 'Context search',
+      ),
+      'Buscar' =>
+      array (
+        0 => 'Search',
+      ),
+      'Centros, Hospitais, Institutos especializados e Museus' =>
+      array (
+        0 => 'Centers, Hospitals, Institutes and Museums',
+      ),
+      'Citações no AMiner' =>
+      array (
+        0 => 'Num. of citations on AMiner',
+      ),
+      'Citações no OpenCitations' =>
+      array (
+        0 => 'Num. of citations on OpenCitations',
+      ),
+      'Clique para uma nova pesquisa' =>
+      array (
+        0 => 'Click here for a new search',
+      ),
+      'Colaboração institucional' =>
+      array (
+        0 => 'Collaboration',
+      ),
+      'Começar novamente' =>
+      array (
+        0 => 'Start over',
+      ),
+      'Como citar' =>
+      array (
+        0 => 'How to cite',
+      ),
+      'Contato' =>
+      array (
+        0 => 'Contact',
+      ),
+      'Data de publicação' =>
+      array (
+        0 => 'Date published',
+      ),
+      'Departamento' =>
+      array (
+        0 => 'Departament',
+      ),
+      'Editora' =>
+      array (
+        0 => 'Publisher',
+      ),
+      'Enviar' =>
+      array (
+        0 => 'Send',
+      ),
+      'Estatísticas da base' =>
+      array (
+        0 => 'Statistics',
+      ),
+      'É parte de ...' =>
+      array (
+        0 => 'Is part of ...',
+      ),
+      'Ferramentas' =>
+      array (
+        0 => 'Tools',
+      ),
+      'Fonte' =>
+      array (
+        0 => 'Source',
+      ),
+      'Idioma' =>
+      array (
+        0 => 'Language',
+      ),
+      'Inglês' =>
+      array (
+        0 => 'English',
+      ),
+      'Início' =>
+      array (
+        0 => 'Home',
+      ),
+      'Limitar por data' =>
+      array (
+        0 => 'Refine with date range',
+      ),
+      'Limitar datas' =>
+      array (
+        0 => 'Refine with date range',
+      ),
+      'Local' =>
+      array (
+        0 => 'Publisher place',
+      ),
+      'Mais informações' =>
+      array (
+        0 => 'More informations',
+      ),
+      'Métricas' =>
+      array (
+        0 => 'Metrics',
+      ),
+      'Métricas do periódico' =>
+      array (
+        0 => 'Journal Metrics',
+      ),
+      'Métricas no nível do artigo' =>
+      array (
+        0 => 'Article level metrics',
+      ),
+      'MONOGRAFIA/LIVRO' =>
+      array (
+        0 => 'Book',
+      ),
+      'Nome do evento' =>
+      array (
+        0 => 'Conference titles',
+      ),
+      'Nossos números' =>
+      array (
+        0 => 'Statistics',
+      ),
+      'Nova pesquisa...' =>
+      array (
+        0 => 'New search...',
+      ),
+      'Número USP' =>
+      array (
+        0 => 'USP ID',
+      ),
+      'Ordenar por Data' =>
+      array (
+        0 => 'Sort by date',
+      ),
+      'Ordenar por Título' =>
+      array (
+        0 => 'Sort by title',
+      ),
+      'ou pesquise este registro no' =>
+      array (
+        0 => 'or search this record in',
+      ),
+      'País das instituições de afiliação dos autores externos' =>
+      array (
+        0 => 'Countries of institutions of collaboration',
+      ),
+      'País de publicação' =>
+      array (
+        0 => 'Countries/Territories',
+      ),
+      'Página Principal' =>
+      array (
+        0 => 'Home',
+      ),
+      'Palavras-chave do autor' =>
+      array (
+        0 => 'Keywords',
+      ),
+      'PARTE DE MONOGRAFIA/LIVRO' =>
+      array (
+        0 => 'BOOK CHAPTER',
+      ),
+      'Pesquisa' =>
+      array (
+        0 => 'Search',
+      ),
+      'Pesquise por termo ou autor' =>
+      array (
+        0 => 'Search for keyword or author',
+      ),
+      'Português' =>
+      array (
+        0 => 'Portuguese',
+      ),
+      'Premiações recebidas' =>
+      array (
+        0 => 'Awards',
+      ),
+      'Produção Científica' =>
+      array (
+        0 => 'Scientific Production',
+      ),
+      'Próxima' =>
+      array (
+        0 => 'Next',
+      ),
+      'Redes sociais' =>
+      array (
+        0 => 'Social Media',
+      ),
+      'Refinar busca' =>
+      array (
+        0 => 'Refine search',
+      ),
+      'registros' =>
+      array (
+        0 => 'records',
+      ),
+      'Resumo' =>
+      array (
+        0 => 'Abstract',
+      ),
+      'Selecione a base' =>
+      array (
+        0 => 'Source selection',
+      ),
+      'Selecionar período de tempo' =>
+      array (
+        0 => 'Select date range',
+      ),
+      'Selecionar os campos para realizar a busca:' =>
+      array (
+        0 => 'Select fields to search:',
+      ),
+      'Selecione uma Unidade USP para filtrar a busca' =>
+      array (
+        0 => 'Select one USP School to filter the search',
+      ),
+      'String de busca avançada' =>
+      array (
+        0 => 'Advanced search string',
+      ),
+      'Sobre' =>
+      array (
+        0 => 'About',
+      ),
+      'TESE' =>
+      array (
+        0 => 'Thesis',
+      ),
+      'Teses e Dissertações' =>
+      array (
+        0 => 'Theses and Dissertations',
+      ),
+      'Termos de busca' =>
+      array (
+        0 => 'Search terms',
+      ),
+      'Tipo de material' =>
+      array (
+        0 => 'Document Types',
+      ),
+      'Tipo de tese' =>
+      array (
+        0 => 'Type',
+      ),
+      'Título' =>
+      array (
+        0 => 'Title',
+      ),
+      'Título da fonte' =>
+      array (
+        0 => 'Source title',
+      ),
+      'Todas as bases' =>
+      array (
+        0 => 'All sources',
+      ),
+      'Todas as Unidades USP' =>
+      array (
+        0 => 'All USP Schools',
+      ),
+      'TRABALHO DE EVENTO' =>
+      array (
+        0 => 'PROCEEDINGS PAPER',
+      ),
+      'TRABALHO DE EVENTO-RESUMO' =>
+      array (
+        0 => 'MEETING ABSTRACT',
+      ),
+      'Últimos registros' =>
+      array (
+        0 => 'Recent submissions',
+      ),
+      'Unidades USP' =>
+      array (
+        0 => 'USP Schools',
+      ),
+      'Usuário' =>
+      array (
+        0 => 'User',
+      ),
+      'Ver registro completo' =>
+      array (
+        0 => 'Full record',
+      ),
+      'Visualização em rede' =>
+      array (
+        0 => 'Network visualization',
+      ),
+    ),
+  ),
+);
