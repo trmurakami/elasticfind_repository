654782e519ff7e53ac6bcaa61b87183f2a25240a
Tiago <tiago@bdpife2.sibi.usp.br>
2019-06-25 14:18:43 -0300

    initial

--- inc/navbar.php
+++ inc/navbar.php
@@ -3,25 +3,25 @@
         <nav class="uk-navbar uk-navbar-container uk-navbar-transparent" data-uk-navbar>
             <div class="uk-navbar-left">
                 <div class="uk-navbar-item uk-padding-remove-horizontal">
-                    <a class="uk-logo" title="BDPI USP" href=""><img src="img/marketing-logo.svg" alt="BDPI USP"></a>
+                    <a class="uk-logo" title="Biblioteca Digital da Produção Intelectual da USP" href="index.php"><img src="img/marketing-logo.svg" alt="Biblioteca Digital da Produção Intelectual da USP"></a>
                 </div>
             </div>
             <div class="uk-navbar-right">
                 <ul class="uk-navbar-nav uk-visible@s">
-                    <li class="uk-active uk-visible@m"><a href="" data-uk-icon="home"></a></li>
-                    <li><a href="">Features</a></li>
+                    <li class="uk-active uk-visible@m"><a href="index.php" data-uk-icon="home"></a></li>
+                    <li><a href="">Busca avançada</a></li>
                     <li>
-                        <a href="#" data-uk-icon="chevron-down">Products</a>
+                        <a href="#" data-uk-icon="chevron-down">SIBI USP</a>
                         <div class="uk-navbar-dropdown">
                             <ul class="uk-nav uk-navbar-dropdown-nav">
-                                <li><a href="#">Big Data</a></li>
-                                <li><a href="#">Marketing</a></li>
-                                <li><a href="#">Analytics</a></li>
-                                <li><a href="#">AI Lab</a></li>
+                                <li><a href="#">Contato</a></li>
+                                <li><a href="#">Sobre</a></li>
+                                <li><a href="#">Tutorial</a></li>
+                                <li><a href="#">Sistema Integrado de Bibliotecas da Universidade de São Paulo</a></li>
                             </ul>
                         </div>
                     </li>
-                    <li><a href="">Testimonials</a></li>
+                    <li><a href="">Login</a></li>
                 </ul>
                 <a class="uk-navbar-toggle uk-navbar-item uk-hidden@s" data-uk-toggle data-uk-navbar-toggle-icon href="#offcanvas-nav"></a>
             </div>
