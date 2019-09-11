965bca28d61ed365148614860985918f8e2805fe
Tiago <tiago@bdpife2.sibi.usp.br>
2019-09-11 08:12:38 -0300

    Install

---
+++ admin/install.php
@@ -0,0 +1,21 @@
+<?php 
+
+require ('../inc/config.php'); 
+
+/* Connect to Elasticsearch */
+try {
+    $client = \Elasticsearch\ClientBuilder::create()->setHosts($hosts)->build(); 
+    //print("<pre>".print_r($client,true)."</pre>");
+    $indexParams['index']  = $index;   
+    $testIndex = $client->indices()->exists($indexParams);
+} catch (Exception $e) {    
+    $error_connection_message = '<div class="alert alert-danger" role="alert">Elasticsearch não foi encontrado. Favor executar o arquivo elasticsearch.lnk.</div>';
+}
+/* Create index if not exists */
+if (isset($testIndex) && $testIndex == false) {
+    Elasticsearch::createIndex($index, $client);
+    Elasticsearch::mappingsIndex($index, $client);
+}
+
+
+?>
