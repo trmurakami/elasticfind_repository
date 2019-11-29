820f98a6ef49c03fa86732263a70e7e107d4384b
Tiago Murakami <trmurakami@gmail.com>
2019-11-29 19:24:26 -0300

    Initial

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="<?php echo "$url_base/"?>">BDPI USP</a>
+        <a class="navbar-brand" href="<?php echo "$url_base/"?>"><?php echo $branch_abrev ?></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
